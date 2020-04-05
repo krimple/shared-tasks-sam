374dcee47079636b94e8372e4def959eb7537e03
Ken Rimple <krimple@chariotsolutions.com>
2020-04-05 12:55:04 -0400

    minor refactor in deployment script and env names

--- scripts/sam-deploy.sh
+++ scripts/sam-deploy.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 scriptdir="${0%/*}"
-sam deploy --parameter-overrides SharedTasksTableArn=`echo $(${scriptdir}/stack-id cognito-stack-dev SharedTasksTableArn)`,TaskManagerRoleName=`echo $(${scriptdir}/stack-id cognito-stack-dev IamUserRole)`
+SHARED_TASKS_TABLE_ARN=`echo $(${scriptdir}/stack-id cognito-stack-dev SharedTasksTableArn)`
+IAM_USER_ROLE=`echo $(${scriptdir}/stack-id cognito-stack-dev IamUserRole)`
+sam deploy --parameter-overrides SharedTasksTableArn=$SHARED_TASKS_TABLE_ARN,TaskManagerRoleName=$IAM_USER_ROLE
