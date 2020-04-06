96885c6493c8786662ca580d1d45fb90d74b2ef4
Ken Rimple <krimple@chariotsolutions.com>
2020-04-06 11:40:44 -0400

    broken code for gw now

--- scripts/sam-deploy.sh
+++ scripts/sam-deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
+AWS_REGION=us-east-1
 scriptdir="${0%/*}"
-SHARED_TASKS_TABLE_ARN=`echo $(${scriptdir}/stack-id cognito-stack-dev SharedTasksTableArn)`
-IAM_USER_ROLE=`echo $(${scriptdir}/stack-id cognito-stack-dev IamUserRole)`
-sam deploy --parameter-overrides SharedTasksTableArn=$SHARED_TASKS_TABLE_ARN,TaskManagerRoleName=$IAM_USER_ROLE
+SHARED_TASKS_TABLE_ARN=`echo $(${scriptdir}/stack-id cognito-stack-dev SharedTasksTableArn)` 
+IAM_USER_ROLE=`echo $(${scriptdir}/stack-id cognito-stack-dev IamUserRole)` 
+sam deploy --region $AWS_REGION --parameter-overrides SharedTasksTableArn=$SHARED_TASKS_TABLE_ARN TaskManagerRoleName=$IAM_USER_ROLE
